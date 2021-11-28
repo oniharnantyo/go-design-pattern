e58bd0512528b7afbfdc7f8283633e9d9583712c
oniharnantyo <oni.harnantyo97@gmail.com>
2021-11-28 13:00:01 +0700

    Add many directories

--- creational/factory/response_generate/response_generate_test.go
+++ creational/factory/response_generate/response_generate_test.go
@@ -3,18 +3,20 @@ package response_generate_test
 import (
 	"testing"
 
-	"../response_generate"
+	"github.com/oniharnantyo/go-design-pattern/creational/factory/response_generate"
 )
 
-func TestJSONGenerate(t *testing.T) {
-	mockData := response_generate.Data{Name: "Bob"}
-	expectedRes := `{"Name":"Bob"}`
+func TestGenerate(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		mockData := response_generate.Data{Name: "Bob"}
+		expectedRes := `{"Name":"Bob"}`
 
-	jsonGenerator := response_generate.NewResponseGenerator(response_generate.JSON)
+		jsonGenerator := response_generate.NewResponseGenerator(response_generate.JSON)
 
-	res := jsonGenerator.Generate(mockData)
+		res := jsonGenerator.Generate(mockData)
 
-	if res != expectedRes {
-		t.Fail()
-	}
+		if res != expectedRes {
+			t.Fail()
+		}
+	})
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/oniharnantyo/go-design-pattern
+
+go 1.16
